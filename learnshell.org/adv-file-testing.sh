24eaffb7410e09222f1778b00ae54d920d372bf5
YC Wong <yengchien@hotmail.com>
2026-06-15 08:58:18 +0800

    Add files via upload
    add adv topics up to File Testing

---
+++ learnshell.org/adv-file-testing.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+filename="sample.md"
+
+# use "-e" to test if file exist
+if [ -e "$filename" ]; then
+    echo "$filename exists as a file"
+fi
+
+# use "-d" to test if directory exists
+directory_name="test_directory"
+if [ -d "$directory_name" ]; then
+    echo "$directory_name exists as a directory"
+fi
+
+
+filename="sample.md"
+if [ ! -f "$filename" ]; then
+    touch "$filename"
+fi
+    
+# use "-r" to test if file has read permission for the user running the script/test
+if [ -r "$filename" ]; then
+    echo "you are allowed to read $filename"
+else
+    echo "you are not allowed to read $filename"
+fi
